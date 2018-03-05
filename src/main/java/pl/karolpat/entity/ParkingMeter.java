458f548b7a5cea92e3a22bf0b39a16c043a58810
karolpat <karol.patecki09@gmail.com>
2018-03-05 21:48:13 +0100

    First entities

--- src/main/java/pl/karolpat/entity/ParkingMeter.java
+++ src/main/java/pl/karolpat/entity/ParkingMeter.java
@@ -24,4 +24,46 @@ public class ParkingMeter {
 	@ManyToOne(fetch=FetchType.EAGER)
 	@JoinColumn(name="parking_id")
 	private User user;
+	
+	
+	//GETTERS AND SETTERS
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Timestamp getStart() {
+		return start;
+	}
+
+	public void setStart(Timestamp start) {
+		this.start = start;
+	}
+
+	public Timestamp getEnd() {
+		return end;
+	}
+
+	public void setEnd(Timestamp end) {
+		this.end = end;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "ParkingMeter [id=" + id + ", start=" + start + ", end=" + end + ", user=" + user + "]";
+	}
+	
+	
 }
